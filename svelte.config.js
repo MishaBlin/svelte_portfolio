40595aef534bfc86e8fe2a669313a390b3af067e
MishaBlin <mikhail_kalinin_a@mail.ru>
2023-07-13 02:48:57 +0300

    Svelte website

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 		adapter: adapter()
 	},
 	paths: {
-		base: process.env.NODE_ENV === 'production' ? '/svelte_portfloio' : ''
+		base: process.env.NODE_ENV === 'production' ? '/svelte_portfolio' : ''
 	}
 };
 
